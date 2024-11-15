48990626adf90c238ffa77bff2fec21fe86fc193
Jonathan Gamble <jdgamble555@gmail.com>
2024-11-15 13:19:10 -0600

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<a class="mt-5 flex flex-col items-center" href="/1">View Todos</a>
---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { resource } from '$lib/resource.svelte';
+
+	type Todo = {
+		id: string;
+		title: string;
+		completed: boolean;
+	};
+
+	const id = $derived($page.params.id);
+
+	const idNumber = $derived(Number(id));
+
+	const todo = resource<Todo>(() =>
+		fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then((response) => response.json())
+	);
+
+	const prevId = $derived(Math.max(idNumber - 1, 1));
+	const nextId = $derived(idNumber + 1);
+</script>
+
+<div class="flex flex-col items-center gap-3">
+	<h1>{todo.value?.title}</h1>
+
+	<div class="flex items-center gap-3">
+		<a class="rounded-lg border p-2" href={prevId.toString()}>Prev</a>
+		<a class="rounded-lg border p-2" href={nextId.toString()}>Next</a>
+	</div>
+</div>
